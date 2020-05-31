dede9f07af9f1c87af1d3904b81579c0e947e68e
nicholastanui@gmail.com <ntanui@safaricom.co.ke>
2020-05-31 19:51:55 +0300

    initialize js scripts

--- script/script.js
+++ script/script.js
@@ -1,9 +1,46 @@
-jQuery(document).ready(function() {
-    jQuery("h2").click(function() {
-      alert("This is a header.");
+$(document).ready(()=> {
+    $("#form-submit form").on("submit", (event)=> {
+        event.preventDefault();
+        let name = $("input#first-name").val();
+        let female = $("input#female").val();
+        let male = $("input#male").val();
+        let day = $("select#day").val();
+        let month = $("select#month").val();
+        let year = $("input#year").val();
+        console.log(`Name: ${name}`);
+        console.log(`Female: ${female}`);
+        console.log(`Male: ${male}`);
+        console.log(`Day: ${day}`);
+        console.log(`Month: ${month}`);
+        console.log(`Year: ${year}`);
+        validateInputs(name, female, male, day, month, year);
     });
-  
-    $("button").click(function() {
-      $(".notification").toggle();
-    });
+    const displayNotification = (text)=> {
+        $('#notification').text("Error! " + text);
+        $("#notification").toggle();
+        setTimeout(() => $("#notification").toggle(), 4000 );
+    }
+    const validateInputs = (name, female, male, day, month, year)=> {
+        if(name == '' || name.length() == 0) {
+            displayNotification(`Kindly indicate you name in the 'First Name' section.`);
+        }
+        if(female == undefined && male == undefined) {
+            displayNotification(`Kindly indicate your gender.`); 
+        }
+    }
+
+    const validateDate = (day, month, year) => {
+        let inputDay = parseInt(day);
+        let inputMonth = parseInt(month);
+        let inputYear = parseInt(year);
+        // Leap year validation
+        if(!isLeapYear(inputYear) && inputMonth > 28) {
+            displayNotification(`The year ${year} is not a leap year!`);
+        }
+        //
+    }
+
+    const isLeapYear = (year) => {
+        return (year%400)?((year%100)?((year%4)?false:true):false):true;
+    }
   });
